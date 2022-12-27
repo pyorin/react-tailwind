80bab98e64be038ce617b3caa1a8c9a75dc951d3
Herlangga Maulani <maulani.morningstar@gmail.com>
2022-12-27 22:35:49 +0700

    komit pertama

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1 className="font-bold text-center mt-5 text-blue-500 text-2xl">
+        React tailwind
+      </h1>
+    </div>
+  );
+}
+
+export default App;
